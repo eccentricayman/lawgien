5c6d166bed9687871a91a73833e33a0141dca637
eccentricayman <aahmed3@stuy.edu>
2016-10-06 15:17:03 -0400

    finished sessions stuff and homepage

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, request
-import csv, hashlib
+from flask import Flask, render_template, request, session, url_for, redirect
+import csv, hashlib, os
 
 app = Flask(__name__)
+app.secret_key = "c9_will_win_worlds"
 
 @app.route("/")
+@app.route("/login")
 def main():
-    print request.headers
-    return render_template("login.html", message = "")
+    if 'logged_in' in session.keys():
+        if session['logged_in']:
+            return redirect(url_for("homepage", message = "Genie says hi, " + session['username'] + "!"))
+        else:
+            return render_template("login.html", message = "")
+    else:
+        return render_template("login.html", message = "")
 
+@app.route("/homepage")
+def homepage():
+    return render_template("homepage.html", uname = session['username'])
+    
 @app.route("/authenticate", methods=['GET', 'POST'])
 def authenticate():
     uname = request.form['username']
@@ -33,14 +44,21 @@ def authenticate():
             if account[0] == hashlib.sha256(uname).hexdigest():
                 if account[1] == hashlib.sha256(pword).digest():
                     data.close()
-                    return render_template("authenticate.html", login = True, message = "Genie is happy that you logged in.")
+                    session['logged_in'] = True
+                    session['username'] = uname
+                    return render_template("authenticate.html", login = True, message = "Genie says hi, " + uname + "!")
                 else:
                     data.close()
                     return render_template("authenticate.html", login = False, message = "Genie is sad that you lied about your password.")
         data.close()
         return render_template("login.html", message = "Genie says you should sign up to be his friend.")
 
-        
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    session.pop('username')
+    return render_template("login.html", message="")
+    
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     app.run()
